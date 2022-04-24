c3e7436c0f086f522c99122af4226d30789105ed
Chris Dukakis <chrisdukakis@gmail.com>
2022-04-24 16:55:13 +0300

    feat: line numbers

--- src/containers/Author.jsx
+++ src/containers/Author.jsx
@@ -3,21 +3,61 @@ import React, { useEffect, useRef, useState } from 'react';
 import hljs from 'highlight.js';
 import extractInterface from 'interface-extractor';
 
-const Editor = styled.pre`
+const Editor = styled.div`
+  display: flex;
+`;
+
+const Source = styled.pre`
   height: ${function (props) {
     return props.height;
   }};
+  max-height: ${function (props) {
+    return props.height;
+  }};
+  width: calc(100vw - 90px);
   white-space: pre;
   word-wrap: normal;
+  overflow-y: scroll;
   overflow-x: auto;
   box-sizing: border-box;
-  padding: 2vh;
+  line-height: 1.25rem;
+  margin: 0;
+
+  &:focus {
+    outline: 0;
+  }
+`;
+
+const LineNumbers = styled.ul`
+  line-height: 1.25rem;
+  list-style-type: none;
+  text-align: right;
+  font-family: monospace;
+  color: rgba(255, 255, 255, 0.7);
+  padding-inline-start: 0;
+  width: 50px;
+  padding: 0 10px 0 0;
+  margin: 0 30px 0 0;
+  overflow: hidden;
+  min-width: ${function (props) {
+    return props.width || '2vh';
+  }};
+  min-height: ${function (props) {
+    return props.height;
+  }};
+  max-height: ${function (props) {
+    return props.height;
+  }};
 `;
 
 const Author = function ({ headerHeight }) {
-  const [source, setSource] = useState();
-  const [position, setPosition] = useState();
+  const [source, setSource] = useState('');
+  const position = useRef();
+  const [top, setTop] = useState();
   const sourceRef = useRef();
+  const linesRef = useRef();
+
+  const lines = source.split(/\r*\n/).length;
 
   // Source: https://stackoverflow.com/a/4812022/6180136
   const getCaretCharacterOffsetWithin = function (element) {
@@ -84,58 +124,87 @@ const Author = function ({ headerHeight }) {
 
   useEffect(
     function () {
-      console.log(position);
-      setCaretPosition(sourceRef.current, position);
+      if (top !== undefined) {
+        sourceRef.current.scrollTo({ top });
+      }
+
+      const extract = async function () {
+        await extractInterface('Test', sourceRef.current.innerText);
+      };
+      extract();
+
+      setCaretPosition(sourceRef.current, position.current);
+
+      sourceRef.current.focus();
     },
-    [source]
+    [source, top]
   );
 
   return (
-    <Editor
-      contentEditable={true}
-      height={`calc(100vh - ${headerHeight}px - 4vh)`}
-      spellCheck={false}
-      onInput={function () {
-        setPosition(getCaretCharacterOffsetWithin(sourceRef.current));
-        setSource(hljs.highlight(sourceRef.current.innerText, { language: 'cpp' }).value);
-      }}
-      onKeyDown={function (event) {
-        console.log(event.code);
-        if (event.code === 'Enter') {
-          event.preventDefault();
-          const docFragment = document.createDocumentFragment();
-          const newEle = document.createElement('br');
-          docFragment.appendChild(newEle);
-          let range = window.getSelection().getRangeAt(0);
-          range.deleteContents();
-          range.insertNode(docFragment);
-          range = document.createRange();
-          range.setStartAfter(newEle);
-          range.collapse(true);
-          const sel = window.getSelection();
-          sel.removeAllRanges();
-          sel.addRange(range);
-
-          const preCaretRange = range.cloneRange();
-          preCaretRange.selectNodeContents(sourceRef.current);
-          preCaretRange.setEnd(range.endContainer, range.endOffset);
-          setPosition(preCaretRange.toString().length + 1);
+    <Editor>
+      <LineNumbers ref={linesRef} height={`calc(100vh - ${headerHeight}px)`}>
+        {[...Array(lines)].map((_, i) => (
+          <li key={i}>{i + 1}</li>
+        ))}
+      </LineNumbers>
+      <Source
+        contentEditable={true}
+        height={`calc(100vh - ${headerHeight}px)`}
+        spellCheck={false}
+        onScroll={() => {
+          linesRef.current.scrollTop = sourceRef.current.scrollTop;
+        }}
+        onInput={function () {
+          setTop(sourceRef.current.scrollTop);
+          position.current = getCaretCharacterOffsetWithin(sourceRef.current);
           setSource(hljs.highlight(sourceRef.current.innerText, { language: 'cpp' }).value);
-        }
-        if (event.code === 'Tab') {
+          sourceRef.current.blur();
+        }}
+        onKeyDown={function (event) {
+          if (event.code === 'Enter') {
+            event.preventDefault();
+            const docFragment = document.createDocumentFragment();
+            const newEle = document.createElement('br');
+            docFragment.appendChild(newEle);
+            if (lines <= 1) {
+              const newEle2 = document.createElement('br');
+              docFragment.appendChild(newEle2);
+            }
+            let range = window.getSelection().getRangeAt(0);
+            range.deleteContents();
+            range.insertNode(docFragment);
+            setTop(undefined);
+            newEle.scrollIntoView();
+            range = document.createRange();
+            range.setStartAfter(newEle);
+            range.collapse(true);
+            const sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+
+            const preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(sourceRef.current);
+            preCaretRange.setEnd(range.endContainer, range.endOffset);
+            position.current = preCaretRange.toString().length + 1;
+            setSource(hljs.highlight(sourceRef.current.innerText, { language: 'cpp' }).value);
+            sourceRef.current.blur();
+          }
+          if (event.code === 'Tab') {
+            event.preventDefault();
+            document.execCommand('insertText', false, '    ');
+          }
+        }}
+        onPaste={function (event) {
           event.preventDefault();
-          document.execCommand('insertText', false, '    ');
-        }
-      }}
-      onPaste={function (event) {
-        event.preventDefault();
-        setPosition(getCaretCharacterOffsetWithin(sourceRef.current));
-        document.execCommand('insertHTML', false, event.clipboardData.getData('text/plain'));
-        setSource(hljs.highlight(sourceRef.current.innerText, { language: 'cpp' }).value);
-      }}
-      ref={sourceRef}
-      dangerouslySetInnerHTML={{ __html: source }}
-    ></Editor>
+          position.current = getCaretCharacterOffsetWithin(sourceRef.current);
+          document.execCommand('insertHTML', false, event.clipboardData.getData('text/plain'));
+          setSource(hljs.highlight(sourceRef.current.innerText, { language: 'cpp' }).value);
+          sourceRef.current.blur();
+        }}
+        ref={sourceRef}
+        dangerouslySetInnerHTML={{ __html: source }}
+      ></Source>
+    </Editor>
   );
 };
 
